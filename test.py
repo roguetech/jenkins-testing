5689e33956a022abcdb75615fc3db631446084ce
roguetech <patrick.roughan@roguetechnology.org>
2020-01-21 12:21:34 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 import boto3, sys, json
 
+ec2 = boto3.client('ec2')
+responce = ec2.describe_instance_attribute(Attribute='instanceType',
+    InstanceId='i-0fffb2393ff39a7ea')
+
+print(response)
+
 print("test python")
